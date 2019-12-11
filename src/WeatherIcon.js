817128ddf109794cbf796649c01d864dde5f4606
nadiacoralves <55755193+nadiacoralves@users.noreply.github.com>
2019-12-11 17:35:58 +0000

    if and else statement working

--- src/WeatherIcon.js
+++ src/WeatherIcon.js
@@ -3,26 +3,43 @@ import { Sunny, Cloudy, Rain, Snow } from "weather-styled-icon";
 import "./weather.css";
 
 export default function WeatherIcon(props) {
-  const codeMapping = {
-    "01d": { Sunny },
-    "01n": { Sunny },
-    "02n": { Sunny },
-    "02d": { Sunny },
-    "03d": { Cloudy },
-    "03n": { Cloudy },
-    "04d": { Cloudy },
-    "04n": { Cloudy },
-    "09d": { Rain },
-    "09n": { Rain },
-    "10d": { Rain },
-    "10n": { Rain },
-    "11d": { Cloudy },
-    "11n": { Cloudy },
-    "13d": { Snow },
-    "13n": { Snow },
-    "50d": { Cloudy },
-    "50d": { Cloudy }
-  };
-
-  return <div icon={codeMapping[props.code]} />;
+  if (props === "01d" || "01n" || "02d" || "02n") {
+    return (
+      <div
+        className="icon"
+        style={{ fontSize: "0.5em", margin: "1em", position: "relative" }}
+      >
+        <Sunny />
+      </div>
+    );
+  }
+  if (props === "09d" || "09n" || "10d" || "10n") {
+    return (
+      <div
+        className="icon"
+        style={{ fontSize: "0.5em", margin: "1em", position: "relative" }}
+      >
+        <Rain />
+      </div>
+    );
+  }
+  if (props === "13d" || "13n") {
+    return (
+      <div
+        className="icon"
+        style={{ fontSize: "0.5em", margin: "1em", position: "relative" }}
+      >
+        <Snow />
+      </div>
+    );
+  } else {
+    return (
+      <div
+        className="icon"
+        style={{ fontSize: "0.5em", margin: "1em", position: "relative" }}
+      >
+        <Cloudy />
+      </div>
+    );
+  }
 }
