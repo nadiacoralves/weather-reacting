41842bacc9172556ff887d1b9eb8c5521d6a64da
nadiacoralves <55755193+nadiacoralves@users.noreply.github.com>
2019-12-16 16:37:43 +0000

    mapping icon

--- src/WeatherIcon.js
+++ src/WeatherIcon.js
@@ -3,44 +3,31 @@ import { Sunny, Cloudy, Rain, Snow } from "weather-styled-icon";
 import "./weather.css";
 
 export default function WeatherIcon(props) {
-  if (props === "01d" || "01n" || "02d" || "02n") {
-    return (
-      <div
-        className="icon"
-        style={{ fontSize: "0.5em", margin: "1em", position: "relative" }}
-      >
-        <Sunny />
-      </div>
-    );
-  } else {
-    if (props === "09d" || "09n" || "10d" || "10n") {
-      return (
-        <div
-          className="icon"
-          style={{ fontSize: "0.5em", margin: "1em", position: "relative" }}
-        >
-          <Rain />
-        </div>
-      );
-    }
-  }
-  if (props === "13d" || "13n") {
-    return (
-      <div
-        className="icon"
-        style={{ fontSize: "0.5em", margin: "1em", position: "relative" }}
-      >
-        <Snow />
-      </div>
-    );
-  } else {
-    return (
-      <div
-        className="icon"
-        style={{ fontSize: "0.5em", margin: "1em", position: "relative" }}
-      >
-        <Cloudy />
-      </div>
-    );
-  }
+  const codeMapping = {
+    "01d": { Sunny },
+    "01n": { Sunny },
+    "02d": { Cloudy },
+    "02n": { Cloudy },
+    "03d": { Cloudy },
+    "04d": { Cloudy },
+    "04n": { Cloudy },
+    "09d": { Rain },
+    "09n": { Rain },
+    "10d": { Rain },
+    "10n": { Rain },
+    "11d": { Rain },
+    "11n": { Rain },
+    "13d": { Snow },
+    "13n": { Snow },
+    "50d": { Rain },
+    "50n": { Rain }
+  };
+  return (
+    <div
+      className="icon"
+      style={{ fontSize: "0.5em", margin: "1em", position: "relative" }}
+    >
+      {codeMapping[props.code]}
+    </div>
+  );
 }
