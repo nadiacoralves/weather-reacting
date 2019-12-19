e761d0c2e44081930971c8748b83a8ba5393b707
nadiacoralves <55755193+nadiacoralves@users.noreply.github.com>
2019-12-19 15:30:37 +0000

    icon attempt again

--- src/WeatherIcon.js
+++ src/WeatherIcon.js
@@ -1,13 +1,22 @@
 import React from "react";
 import { Sunny, Cloudy, Rain, Snow } from "weather-styled-icon";
 import "./weather.css";
+import cloudyDay1 from "./images/cloudyDay1.svg";
+import cloudyDay3 from "./images/cloudyDay3.svg";
+import cloudy from "./images/cloudy.svg";
+import cloudyNight1 from "./images/cloudyNight1.svg";
+import day from "./images/day.svg";
+import night from "./images/night.svg";
+import rainy1 from "./images/rainy1.svg";
+import snowy1 from "./images/snowy1.svg";
+import snowy2 from "./images/snowy2.svg";
 
 export default function WeatherIcon(props) {
+  let icons = [Sunny, Cloudy, Rain, Snow];
+  let iconDays = icons[props.code];
   return (
-    <div
-      className="icon"
-      style={{ fontSize: "0.5em", margin: "1em", position: "relative" }}
-    >
+    <div style={{ fontSize: "0.5em", margin: "1em", position: "relative" }}>
+      {iconDays}
       <Sunny />
     </div>
   );
