e376c1ba6e316cfbbfeef082a874216fc43bc6f8
nadiacoralves <55755193+nadiacoralves@users.noreply.github.com>
2020-01-13 23:15:30 +0000

    if and else statement receiving props

--- src/WeatherIcon.js
+++ src/WeatherIcon.js
@@ -3,12 +3,44 @@ import { Sunny, Cloudy, Rain, Snow } from "weather-styled-icon";
 import "./weather.css";
 
 export default function WeatherIcon(props) {
-  let icons = [Sunny, Cloudy, Rain, Snow];
-  let iconDays = icons[props.code];
-  return (
-    <div style={{ fontSize: "0.5em", margin: "1em", position: "relative" }}>
-      {iconDays}
-      <Sunny />
-    </div>
-  );
+  if (props === "01d" || "01n" || "02d" || "02n") {
+    return (
+      <div
+        className="icon"
+        style={{ fontSize: "0.5em", margin: "1em", position: "relative" }}
+      >
+        <Sunny />
+      </div>
+    );
+  } else {
+    if (props.data === "09d" || "09n" || "10d" || "10n") {
+      return (
+        <div
+          className="icon"
+          style={{ fontSize: "0.5em", margin: "1em", position: "relative" }}
+        >
+          <Rain />
+        </div>
+      );
+    }
+  }
+  if (props.data === "13d" || "13n") {
+    return (
+      <div
+        className="icon"
+        style={{ fontSize: "0.5em", margin: "1em", position: "relative" }}
+      >
+        <Snow />
+      </div>
+    );
+  } else {
+    return (
+      <div
+        className="icon"
+        style={{ fontSize: "0.5em", margin: "1em", position: "relative" }}
+      >
+        <Cloudy />
+      </div>
+    );
+  }
 }
